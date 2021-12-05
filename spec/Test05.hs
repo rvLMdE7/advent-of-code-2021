f677138bce83b43c2442fc0f3153c763b049bc68
rvLMdE7 <75222896+rvLMdE7@users.noreply.github.com>
2021-12-05 18:45:42 +0000

    do day 5 part 2

--- spec/Test05.hs
+++ spec/Test05.hs
@@ -1,67 +1,170 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+{-# OPTIONS_GHC -Wno-orphans #-}
+
 module Test05 where
 
-import Data.Function ((&))
+import Data.List qualified as List
 import Data.Map qualified as Map
 import Data.Maybe (mapMaybe)
 import Data.Text qualified as Text
 import Linear (V2(V2))
+import Optics ((.~), (&))
 import Test.Tasty (TestTree)
 import Test.Tasty qualified as Tasty
 import Test.Tasty.HUnit ((@?=))
 import Test.Tasty.HUnit qualified as HUnit
+import Test.Tasty.QuickCheck (Arbitrary, (===))
+import Test.Tasty.QuickCheck qualified as Check
 
 import Day05 qualified
+import Common
 
+
+instance Arbitrary a => Arbitrary (V2 a) where
+    arbitrary = V2 <$> Check.arbitrary <*> Check.arbitrary
 
 main :: IO ()
 main = Tasty.defaultMain tests
 
 tests :: TestTree
-tests = Tasty.testGroup "tests" [unitTests]
+tests = Tasty.testGroup "tests" [unitTests, propertyTests]
 
 unitTests :: TestTree
 unitTests = Tasty.testGroup "unit tests"
-    [ Tasty.testGroup "cartesianLine"
-        [ HUnit.testCase "example 1" $
-            Day05.cartesianLine @Int (V2 1 1) (V2 1 3)
-                @?= Just (V2 1 1, Right 2)
-        , HUnit.testCase "example 2" $
-            Day05.cartesianLine @Int (V2 9 7) (V2 7 7)
-                @?= Just (V2 7 7, Left 2)
-        , HUnit.testCase "example 3" $
-            Day05.cartesianLine @Int (V2 0 0) (V2 8 8)
-                @?= Nothing
+    [ Tasty.testGroup "straight"
+        [ Tasty.testGroup "straightLine"
+            [ HUnit.testCase "example 1" $
+                Day05.straightLine @Int (V2 1 1) (V2 1 3)
+                    @?= Just (V2 1 1, Right 2)
+            , HUnit.testCase "example 2" $
+                Day05.straightLine @Int (V2 9 7) (V2 7 7)
+                    @?= Just (V2 7 7, Left 2)
+            , HUnit.testCase "example 3" $
+                Day05.straightLine @Int (V2 0 0) (V2 8 8)
+                    @?= Nothing
+            ]
+        , Tasty.testGroup "plotStraightLine"
+            [ HUnit.testCase "example 1" $
+                Day05.plotStraightLine @Int (V2 1 1) (Right 2)
+                    @?= Map.fromList [ (V2 1 y, 1) | y <- [1..3] ]
+            , HUnit.testCase "example 2" $
+                Day05.plotStraightLine @Int (V2 7 7) (Left 2)
+                    @?= Map.fromList [ (V2 x 7, 1) | x <- [7..9] ]
+            ]
         ]
-    , Tasty.testGroup "plotCartesianLine"
-        [ HUnit.testCase "example 1" $
-            Day05.plotCartesianLine @Int (V2 1 1) (Right 2)
-                @?= Map.fromList [ (V2 1 y, 1) | y <- [1..3] ]
-        , HUnit.testCase "example 2" $
-            Day05.plotCartesianLine @Int (V2 7 7) (Left 2)
-                @?= Map.fromList [ (V2 x 7, 1) | x <- [7..9] ]
+    , Tasty.testGroup "diagonal"
+        [ Tasty.testGroup "diagonalLine"
+            [ HUnit.testCase "example 1" $
+                Day05.diagonalLine @Int (V2 1 1) (V2 3 3)
+                    @?= Just (V2 1 1, Left 2)
+            , HUnit.testCase "example 2" $
+                Day05.diagonalLine @Int (V2 9 7) (V2 7 9)
+                    @?= Just (V2 7 9, Right 2)
+            , HUnit.testCase "example 3" $
+                Day05.diagonalLine @Int (V2 0 0) (V2 0 8)
+                    @?= Nothing
+            ]
+        , Tasty.testGroup "plotDiagonalLine"
+            [ HUnit.testCase "example 1" $
+                Day05.plotDiagonalLine @Int (V2 1 1) (Left 2)
+                    @?= Map.fromList [ (V2 s s, 1) | s <- [1..3] ]
+            , HUnit.testCase "example 2" $
+                let result = Map.fromList $ do
+                        s <- [0 .. 2]
+                        pure (V2 7 9 + V2 s (-s), 1)
+                in  Day05.plotDiagonalLine @Int (V2 7 9) (Right 2)
+                        @?= result
+            ]
         ]
-    , HUnit.testCase "prettyGrid" $
-        let actual = exampleLines
-                & mapMaybe (uncurry Day05.cartesianLine)
-                & Day05.plotCartesianLines
-                & Day05.prettyGrid
-            expected = Text.intercalate "\n"
-                [ ".......1.."
-                , "..1....1.."
-                , "..1....1.."
-                , ".......1.."
-                , ".112111211"
-                , ".........."
-                , ".........."
-                , ".........."
-                , ".........."
-                , "222111...."
-                ]
-        in  actual @?= expected
-    , HUnit.testCase "numPointsWith" $
-        Day05.numPointsWith (>= 2) exampleLines @?= 5
+    , Tasty.testGroup "prettyGrid"
+        [ HUnit.testCase "plotStraightLines" $
+            let actual = exampleLines
+                    & mapMaybe (uncurry Day05.straightLine)
+                    & Day05.plotStraightLines
+                    & Day05.prettyGrid
+                expected = Text.intercalate "\n"
+                    [ ".......1.."
+                    , "..1....1.."
+                    , "..1....1.."
+                    , ".......1.."
+                    , ".112111211"
+                    , ".........."
+                    , ".........."
+                    , ".........."
+                    , ".........."
+                    , "222111...."
+                    ]
+            in  actual @?= expected
+        , HUnit.testCase "plotLines" $
+            let actual = Day05.prettyGrid $ Day05.plotLines exampleLines
+                expected = Text.intercalate "\n"
+                    [ "1.1....11."
+                    , ".111...2.."
+                    , "..2.1.111."
+                    , "...1.2.2.."
+                    , ".112313211"
+                    , "...1.2...."
+                    , "..1...1..."
+                    , ".1.....1.."
+                    , "1.......1."
+                    , "222111...."
+                    ]
+            in  actual @?= expected
+        ]
+    , Tasty.testGroup "overlaps"
+        [ HUnit.testCase "plotStraightLines" $
+            let count = exampleLines
+                    & mapMaybe (uncurry Day05.straightLine)
+                    & Day05.plotStraightLines
+                    & Map.filter (>= 2)
+                    & length
+            in  count @?= 5
+        , HUnit.testCase "plotLines" $
+            let count = exampleLines
+                    & Day05.plotLines
+                    & Map.filter (>= 2)
+                    & length
+            in  count @?= 12
+        ]
+    ]
+
+propertyTests :: TestTree
+propertyTests = Tasty.testGroup "property tests"
+    [ Tasty.testGroup "straightLine"
+        [ Check.testProperty "commutative" $
+            \u v -> Day05.straightLine @Int u v === Day05.straightLine v u
+        , Check.testProperty "returns endpoints" $
+            let tryRun = \(p, vec1) ->
+                    let vec2 = case p of
+                            Left x  -> vec1 & _x .~ x
+                            Right y -> vec1 & _y .~ y
+                    in  do  (base, off) <- Day05.straightLine @Int vec1 vec2
+                            pure (vec1, vec2, base, off)
+            in  Check.forAll (Check.arbitrary `Check.suchThatMap` tryRun) $
+                    \(vec1, vec2, base, off) ->
+                        let end = case off of
+                                Left x  -> base + V2 x 0
+                                Right y -> base + V2 0 y
+                        in  List.sort [base, end] === List.sort [vec1, vec2]
+        ]
+    , Tasty.testGroup "diagonalLine"
+        [ Check.testProperty "commutative" $
+            \u v -> Day05.diagonalLine @Int u v === Day05.diagonalLine v u
+        , Check.testProperty "returns endpoints" $
+            let tryRun = \(p, vec1) ->
+                    let vec2 = case p of
+                            Left l  -> vec1 + V2 l l
+                            Right r -> vec1 + V2 r (-r)
+                    in  do  (base, off) <- Day05.diagonalLine @Int vec1 vec2
+                            pure (vec1, vec2, base, off)
+            in  Check.forAll (Check.arbitrary `Check.suchThatMap` tryRun) $
+                    \(vec1, vec2, base, off) ->
+                        let end = case off of
+                                Left l  -> base + V2 l l
+                                Right r -> base + V2 r (-r)
+                        in  List.sort [base, end] === List.sort [vec1, vec2]
+        ]
     ]
 
 exampleLines :: [(V2 Int, V2 Int)]
--- src/Day05.hs
+++ src/Day05.hs
@@ -3,6 +3,7 @@
 module Day05 where
 
 import Control.Arrow ((&&&))
+import Data.Either (partitionEithers)
 import Data.Map (Map)
 import Data.Map qualified as Map
 import Data.Maybe (mapMaybe)
@@ -38,16 +39,35 @@ parseLines = parseLine `Par.sepEndBy` Par.Ch.space
 getLines :: Num a => Text -> Either String [(V2 a, V2 a)]
 getLines = runParser "day-05" $ parseLines <* Par.eof
 
-cartesianLine :: (Num a, Ord a) => V2 a -> V2 a -> Maybe (V2 a, Either a a)
-cartesianLine v1@(V2 x1 y1) v2@(V2 x2 y2)
+-- | The meaning of a return value @Just (base, off)@ is that:
+--
+-- * @off == Left l@ implies a horizontal line - add @l@ to x
+-- * @off == Right r@ implies a vertical line - add @r@ to y
+straightLine :: (Num a, Ord a) => V2 a -> V2 a -> Maybe (V2 a, Either a a)
+straightLine v1@(V2 x1 y1) v2@(V2 x2 y2)
     | (y1 == y2) && (x2 >  x1) = Just (v1, Left  $ x2 - x1)
     | (y1 == y2) && (x2 <= x1) = Just (v2, Left  $ x1 - x2)
     | (x1 == x2) && (y2 >  y1) = Just (v1, Right $ y2 - y1)
     | (x1 == x2) && (y2 <= y1) = Just (v2, Right $ y1 - y2)
     | otherwise                = Nothing
 
-plotCartesianLine :: Integral a => V2 a -> Either a a -> Map (V2 a) Int
-plotCartesianLine vec = \case
+-- | The meaning of a return value @Just (base, off)@ is that:
+--
+-- * @off == Left l@ implies a /positive/ gradient - add @l@ to x and y
+-- * @off == Right r@ implies a /negative/ gradient - add @r@ to x, subtract
+--   it from y
+diagonalLine :: (Num a, Ord a) => V2 a -> V2 a -> Maybe (V2 a, Either a a)
+diagonalLine v1 v2
+    | (x3 ==  y3) && (x3 >  0) = Just (v1, Left x3)
+    | (x3 ==  y3) && (x3 <= 0) = Just (v2, Left $ -x3)
+    | (x3 == -y3) && (x3 >  0) = Just (v1, Right x3)
+    | (x3 == -y3) && (x3 <= 0) = Just (v2, Right $ -x3)
+    | otherwise                = Nothing
+  where
+    V2 x3 y3 = v2 - v1
+
+plotStraightLine :: Integral a => V2 a -> Either a a -> Map (V2 a) Int
+plotStraightLine vec = \case
     Left x -> Map.fromList $ do
         off <- [0 .. x]
         pure (vec & _x +~ off, 1)
@@ -55,9 +75,23 @@ plotCartesianLine vec = \case
         off <- [0 .. y]
         pure (vec & _y +~ off, 1)
 
-plotCartesianLines :: Integral a => [(V2 a, Either a a)] -> Map (V2 a) Int
-plotCartesianLines =
-    fmap (uncurry plotCartesianLine)
+plotDiagonalLine :: Integral a => V2 a -> Either a a -> Map (V2 a) Int
+plotDiagonalLine vec = \case
+    Left l -> Map.fromList $ do
+        off <- [0 .. l]
+        pure (vec + V2 off off, 1)
+    Right r -> Map.fromList $ do
+        off <- [0 .. r]
+        pure (vec + V2 off (-off), 1)
+
+plotStraightLines :: Integral a => [(V2 a, Either a a)] -> Map (V2 a) Int
+plotStraightLines =
+    fmap (uncurry plotStraightLine)
+        .> foldr (Map.unionWith (+)) Map.empty
+
+plotDiagonalLines :: Integral a => [(V2 a, Either a a)] -> Map (V2 a) Int
+plotDiagonalLines =
+    fmap (uncurry plotDiagonalLine)
         .> foldr (Map.unionWith (+)) Map.empty
 
 prettyGrid :: (Enum a, Ord a) => Map (V2 a) Int -> Text
@@ -75,14 +109,29 @@ prettyGrid lattice
     width = maximum $ fmap (textShow .> Text.length) $ Map.elems lattice
     join = if width > 1 then Text.unwords else mconcat
 
-numPointsWith :: Integral a => (Int -> Bool) -> [(V2 a, V2 a)] -> Int
-numPointsWith f = mapMaybe (uncurry cartesianLine)
-    .> plotCartesianLines
-    .> Map.filter f
+part1 :: Integral a => [(V2 a, V2 a)] -> Int
+part1 = mapMaybe (uncurry straightLine)
+    .> plotStraightLines
+    .> Map.filter (>= 2)
     .> length
 
-part1 :: Integral a => [(V2 a, V2 a)] -> Int
-part1 = numPointsWith (>= 2)
+maybeEither :: (a -> Maybe b) -> (a -> Maybe c) -> a -> Maybe (Either b c)
+maybeEither left right x = case left x of
+    Just l  -> Just $ Left l
+    Nothing -> case right x of
+        Just r  -> Just $ Right r
+        Nothing -> Nothing
+
+plotLines :: Integral a => [(V2 a, V2 a)] -> Map (V2 a) Int
+plotLines vecs = Map.unionWith (+)
+    (plotStraightLines straights)
+    (plotDiagonalLines diagonals)
+  where
+    tryLine = maybeEither (uncurry straightLine) (uncurry diagonalLine)
+    (straights, diagonals) = partitionEithers $ mapMaybe tryLine vecs
+
+part2 :: Integral a => [(V2 a, V2 a)] -> Int
+part2 = plotLines .> Map.filter (>= 2) .> length
 
 main :: IO ()
 main = do
@@ -91,3 +140,4 @@ main = do
         Left err -> die err
         Right lines' -> do
             print $ part1 lines'
+            print $ part2 lines'
