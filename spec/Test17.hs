61660e55f5b0e6d8ad44757d90311c5d7b288ab0
rvLMdE7 <75222896+rvLMdE7@users.noreply.github.com>
2021-12-25 17:03:33 +0000

    suppress more warning

--- spec/Test17.hs
+++ spec/Test17.hs
@@ -2,10 +2,10 @@ module Test17 where
 
 import Test.Tasty (TestTree)
 import Test.Tasty qualified as Tasty
-import Test.Tasty.HUnit ((@?=))
-import Test.Tasty.HUnit qualified as HUnit
+-- import Test.Tasty.HUnit ((@?=))
+-- import Test.Tasty.HUnit qualified as HUnit
 
-import Day17 qualified
+-- import Day17 qualified
 
 
 main :: IO ()
